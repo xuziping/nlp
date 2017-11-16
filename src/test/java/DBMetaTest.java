bd24aaa26bfd43b3e9abb08948e19bca76bcfc4e
za-xuzhiping <xuzhiping@zhongan.io>
2017-11-16 16:27:05 +0800

    update

--- src/test/java/DBMetaTest.java
+++ src/test/java/DBMetaTest.java
@@ -1,37 +1,32 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
-import java.util.ArrayList;
+import com.google.common.collect.Lists;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.*;
 import java.util.List;
 
 
 public class DBMetaTest {
     private Connection connection;
     private Statement statement;
-    private String META_COLUMNS = "show columns in city";
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/world?serverTimezone=CTT&useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true";
+
+
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/world?serverTimezone=CTT&useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true";
+    private static final String USERNAME = "root";
+    private static final String PWD = "123456";
+
+    private static final String QUERY_TABLE_COMMENT = "show create table {0}";
+    private static final String QUERY_ALL_TABLE = "show tables";
+    private static final String QUERY_TABLE_METAMETA_COLUMNS = "show columns in {0}";
 
     public static void main(String[] args) throws Exception {
         DBMetaTest test = new DBMetaTest();
-        test.initConnection(JDBC_DRIVER,
-                DB_URL
-                , "root"
-                , "123456");
-        StringBuffer sb = new StringBuffer();
-        ResultSet resultSet = test.getMetaDataTables();
-        List<String> list = new ArrayList<>();
-        while (resultSet.next()) {
-            list.add(resultSet.getString(1));
-        }
+        test.initConnection(JDBC_DRIVER, DB_URL, USERNAME, PWD);
+        List<String> list = test.getMetaDataTables();
         for (int i = 0; i < list.size(); i++) {
             System.out.println("\n" + "table:" + list.get(0) + "\n");
             test.displayMetaData(test.getMetaDataFromTable("sys_user"));
         }
-
-
     }
 
     public void initConnection(String driverClass, String dbUrl, String username, String password) throws Exception {
@@ -40,12 +35,42 @@ public class DBMetaTest {
         this.statement = this.connection.createStatement();
     }
 
-    public ResultSet getMetaDataTables() throws Exception {
-        String sql = "show tables ";
-        ResultSet rs = this.statement.executeQuery(sql);
-        return rs;
+    public List<String> getMetaDataTables() throws Exception {
+        List<String> list = Lists.newArrayList();
+        try(ResultSet rs = this.statement.executeQuery(QUERY_ALL_TABLE)){
+            while (rs.next()) {
+                list.add(rs.getString(1));
+            }
+        }
+        return list;
     }
 
+
+    public String getTableComments() throws Exception {
+        String comment = null;
+        try(ResultSet rs = this.statement.executeQuery(QUERY_TABLE_COMMENT)){
+            if (rs.next()) {
+                comment = rs.getString(2);
+            }
+        }
+        return comment;
+    }
+
+    public static String parseComment(String all) {
+        String comment = null;
+        int index = all.indexOf("COMMENT='");
+        if (index < 0) {
+            return "";
+        }
+        comment = all.substring(index + 9);
+        comment = comment.substring(0, comment.length() - 1);
+        try {
+            comment = new String(comment.getBytes("utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return comment;
+    }
 
     public ResultSetMetaData getMetaDataFromTable(String tableName) throws Exception {
         String sql = "SELECT * FROM " + tableName.toUpperCase() + " WHERE 1 != 1";
